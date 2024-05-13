0e84791586b84fd3d7abfa4540f11bdec5c53a32
Rishabh Gupta <rishabh9aug@gmail.com>
2024-05-13 17:11:08 +0530

    setted up controller

---
+++ src/main/java/com/projects/firstjobapp/JobController.java
@@ -0,0 +1,22 @@
+package com.projects.firstjobapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+@RestController
+public class JobController {
+
+
+    private List<Job> jobs=new ArrayList<>();
+
+    @GetMapping("/jobs")
+    public List<Job> findAll()
+    {
+        return jobs;
+    }
+}
